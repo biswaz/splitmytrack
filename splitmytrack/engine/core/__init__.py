095f257bc2b025ae441f8be632d8996b8e655931
biswaz <biswasb007@gmail.com>
2020-05-10 18:27:42 +0530

    use global separator

--- splitmytrack/engine/core/__init__.py
+++ splitmytrack/engine/core/__init__.py
@@ -2,8 +2,9 @@ import os
 from django.conf import settings
 from spleeter.separator import Separator
 
+separator = Separator('spleeter:2stems')
+
 
 def split_tracks(file_path):
-    separator = Separator('spleeter:2stems')
     separator.separate_to_file(file_path, os.path.join(settings.MEDIA_ROOT, 'processed'))
 
