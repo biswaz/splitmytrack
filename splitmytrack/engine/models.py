3745c486d983e92ed509dc02f691e24f7460572c
biswaz <biswasb007@gmail.com>
2020-05-14 09:15:44 +0530

    model __str__

--- splitmytrack/engine/models.py
+++ splitmytrack/engine/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class TrackUpload(models.Model):
     upload_time = models.DateTimeField(auto_now_add=True)
     file = models.FileField(upload_to='uploads/')
+
+    def __str__(self):
+        return self.file.name
