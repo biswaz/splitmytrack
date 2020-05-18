b0f39f7d4cf60949767d3c5210a3c9ff378a86fc
biswaz <biswasb007@gmail.com>
2020-05-18 16:35:01 +0530

    email console for local dev.

--- config/settings/base.py
+++ config/settings/base.py
@@ -171,7 +171,7 @@ TEMPLATES = [
         # https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-TEMPLATES-BACKEND
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         # https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
-        "DIRS": [str(APPS_DIR / "templates")],
+        "DIRS": [str(APPS_DIR / "ui" / "templates")],
         "OPTIONS": {
             # https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
             # https://docs.djangoproject.com/en/dev/ref/templates/api/#loader-types
