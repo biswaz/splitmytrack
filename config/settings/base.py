ede4291429b6edb1353e02d8055436582b9537c5
biswaz <biswasb007@gmail.com>
2020-05-24 13:55:18 +0530

    change admin email

--- config/settings/base.py
+++ config/settings/base.py
@@ -233,7 +233,7 @@ EMAIL_TIMEOUT = 5
 # Django Admin URL.
 ADMIN_URL = "admin/"
 # https://docs.djangoproject.com/en/dev/ref/settings/#admins
-ADMINS = [("""biswaz""", "biswaz@example.com")]
+ADMINS = [("""biswaz""", "biswasb007@gmail.com")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
